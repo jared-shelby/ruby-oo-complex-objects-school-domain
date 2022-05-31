7d86b2f9168018883f5fb865790fe97e28de9418
Jared Shelby <jared.shelby@yale.edu>
2022-05-31 12:06:49 -0400

    commit tree af9c4ed5c76d317293b0c4accd6646512461fd21

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+ require "pry"
+ class School
+    def initialize(name)
+        @name = name
+        @roster = Hash.new
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade)
+        if roster.include?(grade)
+            @roster[grade] << name
+        else
+            @roster[grade] = []
+            @roster[grade] << name
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each {|grade, names| names.sort!}
+    end
+end
